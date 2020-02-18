28023dc92d06cfc98fde0585e5a6d6517d6438c2
Mateusz Miloszewski <monitorwebdesign@gmail.com>
2020-02-18 10:40:30 +0100

    Added Button.js & Button.module.scss
    Also added Input.js & Input.module.scss

--- src/App.js
+++ src/App.js
@@ -1,66 +1,47 @@
-import React from 'react';
-import ListWrapper from './components/ListWrapper/ListWrapper';
-import Form from './components/Form/Form'
-import './index.css';
+import React from "react";
+import ListWrapper from "./components/ListWrapper/ListWrapper";
+import "./index.css";
+import Form from "./components/Form/Form";
 
 const initialStateItems = [
-    {
-        image: 'https://s3.eu-central-1.amazonaws.com/eduweb-image/blacksquare_01+(1).JPG',
-        name: 'Dan Abramov',
-        twitterLink: 'https://twitter.com/dan_abramov',
-    },
-    {
-        image: 'https://s3.eu-central-1.amazonaws.com/eduweb-image/blacksquare_01+(1).JPG',
-        name: 'Ryan Florence',
-        description: 'Making React accessible for users and developers at https://reach.tech . Online learning, workshops, OSS, and consulting.',
-        twitterLink: 'https://twitter.com/ryanflorence',
-    },
-    {
-        image: 'https://s3.eu-central-1.amazonaws.com/eduweb-image/blacksquare_01+(1).JPG',
-        name: 'Michael Jackson',
-        description: 'Maker. Co-author of React Router. Working on @ReactTraining. Created @unpkg. Head over heels for @cari.',
-        twitterLink: 'https://twitter.com/mjackson',
-    },
-    {
-        image: 'https://s3.eu-central-1.amazonaws.com/eduweb-image/blacksquare_01+(1).JPG',
-        name: 'Kent C. Dodds',
-        description: 'Making software development more accessible · Husband, Father, Latter-day Saint, Teacher, OSS, GDE, @TC39 · @PayPalEng @eggheadio @FrontendMasters · #JS',
-        twitterLink: 'https://twitter.com/kentcdodds',
-    },
+  {
+    image: "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/032/square_480/oapgW_Fp_400x400.jpg",
+    name: "Dan Abramov",
+    description: "The creator of React",
+    twitterLink: "https://twitter.com/dan_abramov"
+  }
 ];
 
-class App extends React.Component { 
-state = {
-items: [...initialStateItems],
-}
+class App extends React.Component {
+  state = {
+    items: [...initialStateItems],
+  };
 
-addItem = (e) => {
-e.preventDefault();
+  addItem = e => {
+    e.preventDefault();
 
-const newItem = {
-    name: e.target[0].value,
-    twitterLink: e.target[1].value,
-    image: e.target[2].value, 
-    description: e.target[3].value,
-}
+    const newItem = {
+      name: e.target[0].value,
+      twitterLink: e.target[1].value,
+      image: e.target[2].value,
+      description: e.target[3].value
+    };
 
-this.setState(prevState => ({
-    items: [...prevState.items, newItem],
-}));
+    this.setState(prevState => ({
+      items: [...prevState.items, newItem]
+    }));
 
-e.target.reset();
-}
+    e.target.reset();
+  };
 
-render() {
-return (
-    <div>
-        <ListWrapper 
-            items={this.state.items}
-        />
+  render() {
+    return (
+      <div>
+        <ListWrapper items={this.state.items} />
         <Form submitFn={this.addItem} />
-    </div>
-)
-}
+      </div>
+    );
+  }
 }
 
 export default App;
